411a12dea3a24a7962a2d429bca34e7fec790647
zakirdpradana <muhammad.zaki54@ui.ac.id>
2026-09-09 21:45:27 +0700

    fix: downgrade django version (Tutorial 2)

--- main/views.py
+++ main/views.py
@@ -9,7 +9,7 @@ def show_main(request):
         "npm": "2506599541",
         "study_program": "S1 Ilmu Komputer",
         "bio": (
-            "Computer Science student at Universitas Indonesia with a solid foundation in mathematics and a keen interest in cybersecurity."
+            "Computer Science student at Universitas Indonesia with a solid foundation in mathematics and a keen interest in cybersecurity. "
             "I enjoy solving problems using a logical and creative approach. I'm eager to learn, grow, and contribute to projects that "
             "create innovative solutions to real-world problem."
         ),
