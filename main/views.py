f4143bd79e896968276f8404eb2c1afa3434c400
zakirdpradana <muhammad.zaki54@ui.ac.id>
2026-09-16 21:12:59 +0700

    feat: add search and delete features in experience section (Tutorial 3)

--- main/views.py
+++ main/views.py
@@ -28,9 +28,19 @@ def show_main(request):
 
 
 def show_experience(request):
+    json_response = get_experience_json(request)
+
+    experiences_data = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+    experiences = [exp.object for exp in experiences_data]
+    title_query = request.GET.get("title", "").strip()
+
     context = {
         "name": "Muhammad Zaki Radipradana",
-        "experience_list": Experience.objects.all(),
+        "experience_list": experiences,
+        "title_query": title_query,
     }
     return render(request, "experience.html", context)
 
@@ -54,3 +64,23 @@ def create_experience(request):
         "form": form,
     }
     return render(request, "experience_form.html", context)
+
+def get_experience_json(request):
+    title_query = request.GET.get("title", "").strip()
+    experiences = Experience.objects.all()
+    
+    if title_query:
+        experiences = experiences.filter(title__icontains=title_query)
+    
+    experience_json = serializers.serialize("json", experiences)
+    return HttpResponse(experience_json, content_type="application/json")
+
+def delete_experience(request, experience_id):
+    experience = get_object_or_404(Experience, pk=experience_id)
+
+    if request.method == "POST":
+        experience.delete()
+        messages.success(request, "Pengalaman berhasil dihapus!")
+        return redirect("main:show_experience")
+    
+    return redirect("main:show_experience")
