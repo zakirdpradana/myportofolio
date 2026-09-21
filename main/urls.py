55c0af4e5de2af6fc54d106ce10f5871f48e4887
zakirdpradana <muhammad.zaki54@ui.ac.id>
2026-09-21 20:07:02 +0700

    feat: get and show education data using JSON (Tugas 3)

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_education, create_experience, get_experience_json, delete_experience, create_education, delete_education
+from main.views import show_main, show_experience, show_education, create_experience, get_experience_json, delete_experience, create_education, delete_education, get_education_json
 
 app_name = "main"
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path("education/", show_education, name="show_education"),
     path("education/add/", create_education, name="create_education"),
     path("education/<uuid:education_id>/delete/",delete_education,name="delete_education"),
+    path("api/education/", get_education_json, name="get_education_json"),
 ]
