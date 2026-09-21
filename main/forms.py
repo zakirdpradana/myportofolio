639592674ec7ea8be9b68e812a1f2101fb6e8087
zakirdpradana <muhammad.zaki54@ui.ac.id>
2026-09-21 17:05:48 +0700

    feat: add modelform and define each data field for education section (Tugas 3)

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,6 @@
-from django.forms import ModelForm, TextInput, Textarea, URLInput, Select, DateTimeInput, CharField, PasswordInput
+from django.forms import ModelForm, TextInput, Textarea, URLInput, Select, DateTimeInput, CharField, PasswordInput, DateInput
 
-from main.models import Experience
+from main.models import Experience, Education
 
 class ExperienceForm(ModelForm):
     secret_key = CharField(
@@ -60,4 +60,62 @@ class ExperienceForm(ModelForm):
                 }
             ),
         }
-        
+
+class EducationForm(ModelForm):
+    secret_key = CharField(
+            label = "Kode Rahasia Admin",
+            widget = PasswordInput(
+                attrs={
+                    "placeholder": "Masukkan kode rahasia"
+                }
+            ),
+            required = True
+        )
+
+    class Meta:
+        model = Education
+        fields = [
+            "institution_name",
+            "description",
+            "thumbnail",
+            "started_at",
+            "ended_at",
+        ]
+
+        labels = {
+            "institution_name": "Nama Institusi Pendidikan",
+            "description": "Deskripsi Pendidikan",
+            "thumbnail": "URL Logo Institusi Pendidikan",
+            "started_at": "Tanggal Mulai",
+            "ended_at": "Tanggal Selesai (Kosongkan jika masih berlangsung)",
+        }
+
+        widgets = {
+            "institution_name": TextInput(
+                attrs={
+                    "placeholder": "SMA 67 Jakarta",
+                    "maxlength": 255,
+                }
+            ),
+            "description": Textarea(
+                attrs={
+                        "placeholder": "Jelaskan program studi, perjalanan, atau pencapaianmu",
+                        "rows": 3,
+                }
+            ),
+            "thumbnail": URLInput(
+                attrs={
+                    "placeholder": "https://drive.google.com/thumbnail?id=...&sz=w1000",
+                }
+            ),
+            "started_at": DateInput(
+                attrs={
+                    "type": "date",
+                }
+            ),
+            "ended_at": DateInput(
+                attrs={
+                    "type": "date",
+                }
+            ),
+        }
